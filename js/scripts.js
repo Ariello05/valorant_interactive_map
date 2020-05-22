3dd89237ce8483effea0f740c4818c789f1051a5
Paweł Dychus <44323692+Ariello05@users.noreply.github.com>
2020-05-22 18:09:59 +0200

    multi source for overlay

--- js/scripts.js
+++ js/scripts.js
@@ -18,14 +18,15 @@ const map_names = {
 }
 
 const map_fetches = {
-  BIND: 'https://api.jsonbin.io/b/5ea03eb55fa47104cea5096d/7',
+  BIND: 'https://api.jsonbin.io/b/5ea03eb55fa47104cea5096d/14',
   SPLIT: '',
   HAVEN: ''
 }
 
-var activeMapButton = $('#map_list_item_bind')
-var activeFilter = $('#filter_li_sage')
-var filterBy = champion_names.SAGE
+let activeMapButton = $('#map_list_item_bind')
+let activeFilter = $('#filter_li_sage')
+let filterBy = champion_names.SAGE
+let imagesArray = []
 
 const get_url_for_map = (map_name) => {
   switch (map_name) {
@@ -58,23 +59,31 @@ const turn_off_modal = () => {
 }
 
 const load_sage = (groups) => {
+  let index = -1
+  imagesArray = []
+
   groups.forEach((group) => {
-    const src = group.source
+    index += 1
+    const srcs = group.source
     const items = group.items
+
+    if (srcs.length === 1) {
+      imagesArray.push([srcs])
+    } else {
+      imagesArray.push([...srcs])
+    }
 
     // eslint-disable-next-line no-undef
     const new_group = d3
       .select('#map_svg')
       .append('g')
       .on('click', () => {
-        turn_on_overlay(src)
+        turn_on_overlay(index)
       })
       .attr('class', 'clickable')
 
     items.forEach((item) => {
-      console.log('halo1')
       if (item.type === 'sageBarrier') {
-        console.log('halo')
         new_group.attr(
           'transform',
           `rotate(${item.rotate} ${item.x + 25} ${item.y + 23})`
@@ -107,6 +116,10 @@ const get_data_processor = () => {
 
   const process_data = (data) => {
     last_used = data
+    if (last_used == null) {
+      return
+    }
+
     switch (filterBy) {
       case champion_names.SAGE:
         load_sage(data.sage)
@@ -134,33 +147,83 @@ const get_data_processor = () => {
 
 const { process_data, refresh_with_data } = get_data_processor()
 
-const turn_on_overlay = (image_to_show_id) => {
-  console.log(image_to_show_id)
+const select_image = (id, index) => {
+  const imagesSize = imagesArray[id].length
+  $('#overlay_container').children('#image_container').children('.card').empty()
+  $('#overlay_container')
+    .children('#image_container')
+    .children('.card')
+    .append(`<img src= resource/${imagesArray[id][index]} />`)
 
+  if (index === 0) {
+    $('#arrow_right')
+      .css('visibility', 'visible')
+      .off('click')
+      .click((ev) => {
+        select_image(id, index + 1)
+      })
+    $('#arrow_left').css('visibility', 'hidden')
+  } else if (index === imagesSize - 1) {
+    $('#arrow_left')
+      .css('visibility', 'visible')
+      .off('click')
+      .click((ev) => {
+        select_image(id, index - 1)
+      })
+    $('#arrow_right').css('visibility', 'hidden')
+  } else {
+    $('#arrow_right')
+      .css('visibility', 'visible')
+      .off('click')
+      .click((ev) => {
+        select_image(id, index + 1)
+      })
+    $('#arrow_left')
+      .css('visibility', 'visible')
+      .off('click')
+      .click((ev) => {
+        select_image(id, index - 1)
+      })
+  }
+}
+
+const turn_on_overlay = (images_id) => {
   $('#overlay_container').children('#image_container').append(
     ` <div class="card">
-    <img src="resource/${image_to_show_id}"/>
+    <img src="resource/${imagesArray[images_id][0]}"/>
     </div>`
   )
+  $('#arrow_left').css('visibility', 'hidden')
+  if (imagesArray[images_id].length > 1) {
+    $('#arrow_right')
+      .css('visibility', 'visible')
+      .off('click')
+      .click((ev) => {
+        select_image(images_id, 1)
+      })
+  } else {
+    $('#arrow_right').css('visibility', 'hidden')
+  }
 
   $('#overlay').fadeIn(200)
 }
+
 const turn_off_overlay = (delay = 200) => {
   $('#overlay').fadeOut(delay)
   $('#overlay_container').children('#image_container').empty()
 }
 
 const put_render_loading_title = () => {
-  $('#interactive_space').append('<span>Loading...</span>')
+  // $('#interactive_space').append('<span>Loading...</span>')
 }
 
 const clear_render_loading_title = () => {
-  $('#interactive_space').empty('span')
+  // $('#interactive_space').empty('span')
 }
 
 const handle_data_fetch = (map_name) => {
   fade_start(120, put_render_loading_title)
-  //IDC what you do with this key anyway...
+  // IDC what you do with this key anyway...
   return fetch(get_url_for_map(map_name), {
     headers: {
       'secret-key':
@@ -200,11 +263,13 @@ const get_fetcher = () => {
         break
       case map_names.SPLIT:
         fade_start(120, () => {
+          process_data(null)
           fade_end(120, map_names.SPLIT)
         })
         break
       case map_names.HAVEN:
         fade_start(120, () => {
+          process_data(null)
           fade_end(120, map_names.HAVEN)
         })
         break
@@ -232,7 +297,7 @@ const fade_end = (time, map) => {
 }
 
 $(document).ready(() => {
-  //update_map('bind')
+  // update_map('bind')
   $('#map_list_item_split').click((ev) => {
     update_map('split')
   })
