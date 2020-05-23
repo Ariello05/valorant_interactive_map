5ddddaefa17a8a7dafb46feb254794b5667b14c0
Paweł Dychus <panmarchewka5@gmail.com>
2020-05-23 03:55:05 +0200

    modules

---
+++ js/data_loader.js
@@ -0,0 +1,172 @@
+let imagesArray = []
+
+const errors = {
+  argumentError: 'Invalid argument in a function call',
+  notImplementedError: "This functionality wasnt't implemented"
+}
+
+const champion_names = {
+  SAGE: 'sage',
+  SOVA: 'sova',
+  BRIMSTONE: 'brimstone'
+}
+
+const select_image = (id, index) => {
+  const imagesSize = imagesArray[id].length
+  $('#overlay_container').children('#image_container').children('.card').empty()
+  $('#overlay_container')
+    .children('#image_container')
+    .children('.card')
+    .append(`<img src= resource/${imagesArray[id][index]} />`)
+
+  if (index === 0) {
+    $('#arrow_right')
+      .css('visibility', 'visible')
+      .off('click')
+      .click((ev) => {
+        select_image(id, index + 1)
+      })
+    $('#arrow_left').css('visibility', 'hidden')
+  } else if (index === imagesSize - 1) {
+    $('#arrow_left')
+      .css('visibility', 'visible')
+      .off('click')
+      .click((ev) => {
+        select_image(id, index - 1)
+      })
+    $('#arrow_right').css('visibility', 'hidden')
+  } else {
+    $('#arrow_right')
+      .css('visibility', 'visible')
+      .off('click')
+      .click((ev) => {
+        select_image(id, index + 1)
+      })
+    $('#arrow_left')
+      .css('visibility', 'visible')
+      .off('click')
+      .click((ev) => {
+        select_image(id, index - 1)
+      })
+  }
+}
+
+const turn_on_overlay = (images_id) => {
+  $('#overlay_container').children('#image_container').append(
+    ` <div class="card">
+    <img src="resource/${imagesArray[images_id][0]}"/>
+    </div>`
+  )
+  $('#arrow_left').css('visibility', 'hidden')
+  if (imagesArray[images_id].length > 1) {
+    $('#arrow_right')
+      .css('visibility', 'visible')
+      .off('click')
+      .click((ev) => {
+        select_image(images_id, 1)
+      })
+  } else {
+    $('#arrow_right').css('visibility', 'hidden')
+  }
+
+  $('#overlay').fadeIn(200)
+}
+
+const turn_off_overlay = (delay = 200) => {
+  $('#overlay').fadeOut(delay)
+  $('#overlay_container').children('#image_container').empty()
+}
+
+const load_sage = (groups) => {
+  let index = -1
+  imagesArray = []
+
+  groups.forEach((group) => {
+    index += 1
+    const srcs = group.source
+    const items = group.items
+
+    if (srcs.length === 1) {
+      imagesArray.push([srcs])
+    } else {
+      imagesArray.push([...srcs])
+    }
+
+    // eslint-disable-next-line no-undef
+    const new_group = d3
+      .select('#map_svg')
+      .append('g')
+      .on('click', () => {
+        turn_on_overlay(index)
+      })
+      .attr('class', 'clickable')
+
+    items.forEach((item) => {
+      if (item.type === 'sageBarrier') {
+        new_group.attr(
+          'transform',
+          `rotate(${item.rotate} ${item.x + 25} ${item.y + 23})`
+        )
+
+        new_group
+          .append('image')
+          .attr('x', `${item.x}`)
+          .attr('y', `${item.y}`)
+          .attr('width', 50)
+          .attr('height', 56)
+          .attr('xlink:href', 'resource/sage_barrier.png')
+
+        new_group
+          .append('rect')
+          .attr('x', `${item.x - 4}`)
+          .attr('y', `${item.y + 5}`)
+          .attr('rx', 5)
+          .attr('ry', 5)
+          .attr('width', 58)
+          .attr('height', 45)
+          .attr('class', `${item.class}`)
+      }
+    })
+  })
+}
+
+const get_data_processor = () => {
+  let last_used = {}
+
+  const process_data = (data, filterBy) => {
+    last_used = data
+    if (last_used == null) {
+      return
+    }
+
+    switch (filterBy) {
+      case champion_names.SAGE:
+        load_sage(data.sage)
+        break
+
+      case champion_names.SOVA:
+        groups = data.sova
+        break
+
+      case champion_names.BRIMSTONE:
+        groups = data.brimstone
+        break
+
+      default:
+        throw new Error(errors.notImplementedError)
+    }
+  }
+  const refresh_with_data = () => {
+    if (!$.isEmptyObject(last_used)) {
+      process_data(last_used)
+    }
+  }
+  return { process_data, refresh_with_data }
+}
+export {
+  get_data_processor,
+  turn_off_overlay,
+  turn_on_overlay,
+  champion_names,
+  errors
+}
--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,11 @@
 /* eslint-disable camelcase */
-
-const errors = {
-  argumentError: 'Invalid argument in a function call',
-  notImplementedError: "This functionality wasnt't implemented"
-}
-
-const champion_names = {
-  SAGE: 'sage',
-  SOVA: 'sova',
-  BRIMSTONE: 'brimstone'
-}
+import {
+  get_data_processor,
+  turn_off_overlay,
+  turn_on_overlay,
+  champion_names,
+  errors
+} from './data_loader.js'
 
 const map_names = {
   BIND: 'bind',
@@ -26,7 +22,6 @@ const map_fetches = {
 let activeMapButton = $('#map_list_item_bind')
 let activeFilter = $('#filter_li_sage')
 let filterBy = champion_names.SAGE
-let imagesArray = []
 
 const get_url_for_map = (map_name) => {
   switch (map_name) {
@@ -58,160 +53,7 @@ const turn_off_modal = () => {
   $('#modal').fadeOut(200)
 }
 
-const load_sage = (groups) => {
-  let index = -1
-  imagesArray = []
-
-  groups.forEach((group) => {
-    index += 1
-    const srcs = group.source
-    const items = group.items
-
-    if (srcs.length === 1) {
-      imagesArray.push([srcs])
-    } else {
-      imagesArray.push([...srcs])
-    }
-
-    // eslint-disable-next-line no-undef
-    const new_group = d3
-      .select('#map_svg')
-      .append('g')
-      .on('click', () => {
-        turn_on_overlay(index)
-      })
-      .attr('class', 'clickable')
-
-    items.forEach((item) => {
-      if (item.type === 'sageBarrier') {
-        new_group.attr(
-          'transform',
-          `rotate(${item.rotate} ${item.x + 25} ${item.y + 23})`
-        )
-
-        new_group
-          .append('image')
-          .attr('x', `${item.x}`)
-          .attr('y', `${item.y}`)
-          .attr('width', 50)
-          .attr('height', 56)
-          .attr('xlink:href', 'resource/sage_barrier.png')
-
-        new_group
-          .append('rect')
-          .attr('x', `${item.x - 4}`)
-          .attr('y', `${item.y + 5}`)
-          .attr('rx', 5)
-          .attr('ry', 5)
-          .attr('width', 58)
-          .attr('height', 45)
-          .attr('class', `${item.class}`)
-      }
-    })
-  })
-}
-
-const get_data_processor = () => {
-  let last_used = {}
-
-  const process_data = (data) => {
-    last_used = data
-    if (last_used == null) {
-      return
-    }
-
-    switch (filterBy) {
-      case champion_names.SAGE:
-        load_sage(data.sage)
-        break
-
-      case champion_names.SOVA:
-        groups = data.sova
-        break
-
-      case champion_names.BRIMSTONE:
-        groups = data.brimstone
-        break
-
-      default:
-        throw new Error(errors.notImplementedError)
-    }
-  }
-  const refresh_with_data = () => {
-    if (!$.isEmptyObject(last_used)) {
-      process_data(last_used)
-    }
-  }
-  return { process_data, refresh_with_data }
-}
-
 const { process_data, refresh_with_data } = get_data_processor()
-
-const select_image = (id, index) => {
-  const imagesSize = imagesArray[id].length
-  $('#overlay_container').children('#image_container').children('.card').empty()
-  $('#overlay_container')
-    .children('#image_container')
-    .children('.card')
-    .append(`<img src= resource/${imagesArray[id][index]} />`)
-
-  if (index === 0) {
-    $('#arrow_right')
-      .css('visibility', 'visible')
-      .off('click')
-      .click((ev) => {
-        select_image(id, index + 1)
-      })
-    $('#arrow_left').css('visibility', 'hidden')
-  } else if (index === imagesSize - 1) {
-    $('#arrow_left')
-      .css('visibility', 'visible')
-      .off('click')
-      .click((ev) => {
-        select_image(id, index - 1)
-      })
-    $('#arrow_right').css('visibility', 'hidden')
-  } else {
-    $('#arrow_right')
-      .css('visibility', 'visible')
-      .off('click')
-      .click((ev) => {
-        select_image(id, index + 1)
-      })
-    $('#arrow_left')
-      .css('visibility', 'visible')
-      .off('click')
-      .click((ev) => {
-        select_image(id, index - 1)
-      })
-  }
-}
-
-const turn_on_overlay = (images_id) => {
-  $('#overlay_container').children('#image_container').append(
-    ` <div class="card">
-    <img src="resource/${imagesArray[images_id][0]}"/>
-    </div>`
-  )
-  $('#arrow_left').css('visibility', 'hidden')
-  if (imagesArray[images_id].length > 1) {
-    $('#arrow_right')
-      .css('visibility', 'visible')
-      .off('click')
-      .click((ev) => {
-        select_image(images_id, 1)
-      })
-  } else {
-    $('#arrow_right').css('visibility', 'hidden')
-  }
-
-  $('#overlay').fadeIn(200)
-}
-
-const turn_off_overlay = (delay = 200) => {
-  $('#overlay').fadeOut(delay)
-  $('#overlay_container').children('#image_container').empty()
-}
 
 const put_render_loading_title = () => {
   // $('#interactive_space').append('<span>Loading...</span>')
@@ -232,7 +74,7 @@ const handle_data_fetch = (map_name) => {
   })
     .then((response) => response.json())
     .then((result) => {
-      process_data(result)
+      process_data(result, filterBy)
       fade_end(120, map_names.BIND, clear_render_loading_title)
       return result
     })
@@ -255,7 +97,7 @@ const get_fetcher = () => {
           })
         } else {
           fade_start(120, () => {
-            process_data(bind_data)
+            process_data(bind_data, filterBy)
             fade_end(120, map_names.BIND)
           })
         }
@@ -263,13 +105,13 @@ const get_fetcher = () => {
         break
       case map_names.SPLIT:
         fade_start(120, () => {
-          process_data(null)
+          process_data(null, filterBy)
           fade_end(120, map_names.SPLIT)
         })
         break
       case map_names.HAVEN:
         fade_start(120, () => {
-          process_data(null)
+          process_data(null, filterBy)
           fade_end(120, map_names.HAVEN)
         })
         break
